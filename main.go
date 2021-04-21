d125bcdb339edec6cfcc4cf16fca399bc120d3fc
Asger Noer <noer.asger@icloud.com>
2021-04-21 17:40:59 +0200

    Main function
    Creates new instanse of data storage layer and passes that object to the handler.

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/AsgerNoer/Todo-service/data"
+	"github.com/AsgerNoer/Todo-service/web"
+)
+
+func main() {
+	log.Println("Service starting")
+
+	store, err := data.NewStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := web.NewHandler(store)
+
+	log.Fatal(http.ListenAndServe(":3000", handler.Router))
+}
